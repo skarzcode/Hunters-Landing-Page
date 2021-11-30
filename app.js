fcced51fbc9902d4c570d516b8eb4efda8e301cb
Abdi <letsbuildshhopify>
2021-11-30 04:11:21 +0000

    nav error

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ var navBarLinks = document.getElementsByClassName("navbar-links")[0]
 toggleButton.addEventListener("click", () => {
     navBarLinks.classList.toggle("active")
 });
+
+window.addEventListener("resize", function() {
+    if (window.matchMedia("(min-width: 400px)").matches) {
+      console.log("Screen width is at least 500px")
+    } else {
+      console.log("Screen less than 500px")
+    }
+  })
